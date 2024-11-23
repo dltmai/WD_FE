e7425b0b4a092908dd34c52cef1405d8e7de020d
tuyetmai <duongmai6241@gmail.com>
2024-11-24 02:01:49 +0700

    add navigation

--- sources/Experience/assets.js
+++ sources/Experience/assets.js
@@ -8,6 +8,22 @@ export default [
         source: "/assets/bakedNeutral.jpg",
         type: "texture",
       },
+      {
+        name: "topChairModel",
+        source: "/assets/topChairModel.glb",
+        type: "model",
+      },
+      {
+        name: "pcScreenModel",
+        source: "/assets/pcScreenModel.glb",
+        type: "model",
+      },
+
+      {
+        name: "macScreenModel",
+        source: "/assets/macScreenModel.glb",
+        type: "model",
+      },
       { name: "roomModel", source: "/assets/roomModel.glb" },
     ],
   },
